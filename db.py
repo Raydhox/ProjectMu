d71dc107f782558006e8281356735c5821aa7d16
Raydhox <38498323+Raydhox@users.noreply.github.com>
2018-11-18 16:53:14 +0100

    Add files via upload

---
+++ db.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+TILE = 32
+
+#COLOR
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
+WHITE = (255, 255, 255)
+
+RED = (255, 0, 0)
+LIME = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+YELLOW = (255, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+
+NAVAJOWHITE = (244, 164, 96)
+DARKGOLDENROD = (218, 165, 32)
+
+SKYBLUE = (135, 206, 235)
+DODGERBLUE = (30, 144, 255)
